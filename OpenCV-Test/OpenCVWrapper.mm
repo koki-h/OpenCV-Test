90fc2225480c6cfebefa91f430e18e61707df146
koki <gohannnotomo@gmail.com>
2019-01-07 17:35:16 +0900

    スイッチ切り替えで二値化した画像と境界線を描画した画像を切り替えできるようにした

--- OpenCV-Test/OpenCVWrapper.mm
+++ OpenCV-Test/OpenCVWrapper.mm
@@ -21,15 +21,17 @@ OpenCVWrapper() <CvVideoCameraDelegate> {
 @implementation OpenCVWrapper
 
 - (void)processImage:(cv::Mat &)image {
-        cv::Mat image_copy;
-//    cv::cvtColor(image, image_copy, CV_BGRA2BGR);
-//    //invert imege
-//    cv::bitwise_not(image_copy, image_copy);
+    cv::Mat image_copy;
     int l_threshold = [[_param objectForKey: @"l_threshold"] intValue];
-
+    bool mode_binalized = [[_param objectForKey:@"mode_binalized"] boolValue];
     image_copy = [self binarizeByLightness:image l_threshold:l_threshold];
-    image = [self drawContours:image_copy canvas:image];
-    cv::cvtColor(image, image, CV_BGR2BGRA);
+    if (mode_binalized) {
+        cv::cvtColor(image_copy, image, CV_BGR2BGRA); //二値化した白黒画像を表示
+    } else {
+        // しきい値の境界線を描画して表示
+        image = [self drawContours:image_copy canvas:image];
+        cv::cvtColor(image, image, CV_BGR2BGRA);
+    }
 }
 
 - (void) createCameraWithParentView:(UIImageView*) parentView {
--- OpenCV-Test/ViewController.swift
+++ OpenCV-Test/ViewController.swift
@@ -11,6 +11,8 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var imgView: UIImageView!
+    @IBOutlet weak var swMode: UISwitch!
+    @IBOutlet weak var slThresholdLight: UISlider!
     let openCv = OpenCVWrapper()
     
     override func viewDidLoad() {
@@ -18,6 +20,9 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
         openCv.createCamera(withParentView: imgView)
         openCv.param["l_threshold"] = 128
+        openCv.param["mode_binalized"] = false
+        swMode.isOn = openCv.param["mode_binalized"] as! Bool
+        slThresholdLight.value = Float(openCv.param["l_threshold"] as! Int)
     }
 
     override func viewDidAppear(_ animated: Bool) {
@@ -26,7 +31,11 @@ class ViewController: UIViewController {
     }
     
     @IBAction func threshold_l_changed(_ sender: UISlider) {
-        openCv.param["l_threshold"] = sender.value
+        openCv.param["l_threshold"] = Int(sender.value)
+    }
+    
+    @IBAction func switchMode(_ sender: UISwitch) {
+        openCv.param["mode_binalized"] = sender.isOn
     }
     
     @IBAction func tapCameraSwitch(_ sender: Any) {
